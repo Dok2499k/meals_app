8717ef27b3985aec9faa38cc3b173a1db5c2eb3e
Dok2499k <kanaev.d.killer@gmail.com>
2024-01-28 23:07:49 +0500

    finish app

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -71,7 +71,7 @@ class MealDetailScreen extends StatelessWidget {
               ListView.builder(
                 itemCount: selectedMeal.ingredients.length,
                 itemBuilder: (ctx, index) => Card(
-                  color: Theme.of(context).accentColor,
+                  color: Theme.of(context).colorScheme.secondary,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                       vertical: 5,
